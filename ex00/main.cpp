28623ae8cfb4baf12e42f6ce7a3de5961f33f2f1
hmouis <houdayfamouis@gmail.com>
2025-12-18 11:53:00 +0100

    Add ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,8 +15,20 @@
 int main()
 {
     Zombie *zom;
-    randomChump("zombie0");
-    zom = newZombie("zombie1");
+    randomChump("random zombie0");
+    randomChump("random zombie1");
+    randomChump("random zombie2");
+    randomChump("random zombie3");
+    zom = newZombie("new zombie1");
+    zom->announce();
+    delete zom;
+    zom = newZombie("new zombie2");
+    zom->announce();
+    delete zom;
+    zom = newZombie("new zombie3");
+    zom->announce();
+    delete zom;
+    zom = newZombie("new zombie4");
     zom->announce();
     delete zom;
     
